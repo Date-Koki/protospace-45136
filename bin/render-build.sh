e6857e6b6d95ee20609ebd3c4d1a8b51fbbb8ff1
Date-Koki <k.date.0320@gmail.com>
2025-08-21 23:06:13 +0900

    third_commit

--- bin/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Render が参照する build スクリプト（Rails 用）
-
-# データベースがあればマイグレーション
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
-
-# 必要ならアセットプリコンパイル
-bundle exec rake assets:precompile
